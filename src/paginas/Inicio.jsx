e63b7b1b648ece6caa18ea5e81bbca9ce4314d06
pengyi-labs <lastresmoyallc@gmail.com>
2022-02-11 01:01:58 -0300

    deploy

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -6,7 +6,7 @@ const Inicio = () => {
   useEffect(() => {
     const obtenerClientesAPI = async () => {
       try {
-        const url = "http://localhost:4000/clientes";
+        const url = import.meta.env.VITE_API_URL;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
         setClientes(resultado);
@@ -20,7 +20,7 @@ const Inicio = () => {
     if (confirmar) {
       try {
         //Editando Registro
-        const url = `http://localhost:4000/clientes/${id}`;
+        const url = `${import.meta.env.VITE_API_URL}/${id}`;
         const respuesta = await fetch(url, {
           method: "DELETE",
         })
